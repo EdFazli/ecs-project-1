ee10525aa6a9c81e40c72677b0e54506a913603d
Syed Fazli <edfazli92@gmail.com>
2022-04-23 06:57:39 +0800

    adding app.py

--- app.py
+++ app.py
@@ -6,5 +6,5 @@ app = Flask(__name__)
 def hello():
     return "Welcome to ECS Test Page!"
 
-    if __name__ == '__main__':
-        app.run(host="0.0.0.0", port=80)
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=80)
